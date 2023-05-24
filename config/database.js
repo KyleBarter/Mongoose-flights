9526cb8243793849cca3fc2b467e74d7c84d0164
KyleBarter <kylebarter4@gmail.com>
2023-05-24 16:32:57 +0100

    part 1 almost complete

--- config/database.js
+++ config/database.js
@@ -1,1 +1,9 @@
 const mongoose = require('mongoose')
+
+
+async function connectDB() {
+    await mongoose.connect(process.env.DATABASE_URL)
+    const db = mongoose.connection
+    console.log(`database connected on ${db.name} on ${db.port}`)
+}
+connectDB()
---
+++ model/flight.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose')
+
+const Schema = mongoose.Schema
+
+const flightSchema = new Schema ({
+    airline: {
+        type: String, 
+        enum: ['British Airways', 'EasyJet', 'RyanAir']
+    },
+    airport: {
+        type: String,
+        enum: ['DXB', 'LGW', 'CPH', 'ARN', 'EDI'],
+        default: 'LHR'
+    },
+    flightNo: {
+        type: Number,
+        require: true,
+        // min & max number 10 & 9999 may need to go elsewhere 
+        min: 10,
+        max: 9999
+    },
+    departs: Date
+})
